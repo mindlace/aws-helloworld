712c85e51152315f001f331425876ab07c6c8440
Ethan Fremen <i@mindlace.net>
2013-06-09 22:06:25 -0700

    Add hello world rails app.
    Signed-off-by: Ethan Fremen <i@mindlace.net>

---
+++ helloworld/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = 'b05a7b1c69ed84d7d3e2ff96558353dad6da764d3553a465eb670a8b469006dcef473254c2c8a5c96998569ee782c7388a0d69d02252188debf7b7a62e2d28f8'
